2b80333d882abbed1bebf99327a1a7e6f4ad2277
rohithreddy1305 <106958996+rohithreddy1305@users.noreply.github.com>
2022-06-09 16:49:41 +0530

    Update chessBoardCornersDetector_pybind.cpp

--- src/chessBoardCornersDetector_pybind.cpp
+++ src/chessBoardCornersDetector_pybind.cpp
@@ -8,13 +8,11 @@ std::string TestBinding(){
     return "Hello Binders!";
 }
 
-PYBIND11_MODULE(chessBoardCornersDetector_pybind, module) {
+PYBIND11_MODULE(pyChessBoardDetector, module) {
     module.doc() = "pybind11 plugin for detecting corners in checkerboard using new OpenCV Algorithm.";
 
     module.def("test_bindings",&TestBinding,"Test Binding Function");
-    module.def("chessBoardCornersDetector", &chessBoardCornersDetector, 
+    module.def("get_chessboard_corners", &getChessBoardCorners, 
     "chessBoardCornersDetector", 
      py::arg("image_path"), py::arg("board_width"), py::arg("board_height"), py::arg("verbose") = false);
 }
-
-
