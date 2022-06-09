15e37012a47d0e681028c7bbd249fbad672c69ec
rohithreddy1305 <106958996+rohithreddy1305@users.noreply.github.com>
2022-06-09 16:51:31 +0530

    Update chessBoardCornersDetector.cpp

--- src/chessBoardCornersDetector.cpp
+++ src/chessBoardCornersDetector.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <list>
+#include <tuple>
 #include <fstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -10,26 +12,31 @@ using namespace cv;
 
 cv::Size boardSize;
 
-Result chessBoardCornersDetector(std::string image_path, int board_width, int board_height,const bool verbose){
+
+list<tuple<float,float>> getChessBoardCorners(std::string image_path, int board_width, int board_height,const bool verbose){
     
-    // cout<<"function called"<<std::endl;
     cv::Mat image = cv::imread(image_path);
-    // cout<<"Board Size:"<<boardSize<<std::endl;
+    boardSize.width = board_width;
+    boardSize.height = board_height;
     CornerDetection::Chessboard chessboard(boardSize, image);
     chessboard.findCorners(false);
 
     std::vector<cv::Point2f> corners_detected;
-    Result result;
+    list<tuple<float,float>> corners_list(board_height*board_width,make_tuple(0,0));
 
     bool ret = chessboard.cornersFound();
-    // cout<<"Flag:"<<ret<<std::endl;
-    result.flag = ret;
-    
+
     if (ret)
     {
         corners_detected = chessboard.getCorners();
-        result.corner = corners_detected;
         cout << "# INFO: Detected chessboard in image " << image_path << std::endl;
+
+        int i=0;
+        for(auto it = corners_list.begin();it!=corners_list.end();it++){
+            get<0>(*it)= corners_detected[i].x;
+            get<1>(*it)= corners_detected[i].y;
+            i++;
+        }
 
         cv::Mat sketch;
         chessboard.getSketch().copyTo(sketch);
@@ -39,7 +46,7 @@ Result chessBoardCornersDetector(std::string image_path, int board_width, int bo
     {
         cout << "# ERROR: Did not detect chessboard in image " << image_path << std::endl;
     }
-    return result;
+    return corners_list;
 
 }
 
@@ -51,9 +58,7 @@ int main(int argc,char** argv){
     boardSize.width = stoi(argv[2]);
     boardSize.height = stoi(argv[3]);
 
-    cout<<"MAIN function complied "<<boardSize.width<<" "<<boardSize.height<<" IMAGE PATH: "<<image_path<<std::endl;
-
-    Result result = chessBoardCornersDetector(image_path,boardSize.width,boardSize.height,false);
+    list<tuple<float,float>> result = getChessBoardCorners(image_path,boardSize.width,boardSize.height,false);
 
     return 0;
 
