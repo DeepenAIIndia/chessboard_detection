98fe6e417a4b9160e108d032352449a4d11957b7
rohithreddy1305 <106958996+rohithreddy1305@users.noreply.github.com>
2022-06-09 19:00:56 +0530

    Update chessBoardCornersDetector.cpp

--- src/chessBoardCornersDetector.cpp
+++ src/chessBoardCornersDetector.cpp
@@ -10,37 +10,40 @@ using namespace std;
 using namespace cv;
 
 
-list<tuple<float,float>> getChessBoardCorners(std::string image_path, int board_width, int board_height,const bool verbose){
-    
-    cv::Size boardSize;
-    cv::Mat image = cv::imread(image_path);
-    boardSize.width = board_width;
-    boardSize.height = board_height;
-    CornerDetection::Chessboard chessboard(boardSize, image);
-    chessboard.findCorners(false);
 
-    std::vector<cv::Point2f> corners_detected;
-    list<tuple<float,float>> corners_list(board_height*board_width,make_tuple(0,0));
+list<list<tuple<float,float>>> getChessBoardCorners(list<std::string> images_path, int board_width, int board_height,const bool verbose){
+    list<list<tuple<float,float>>> ans;
+    for(auto it1=images_path.begin();it1!=images_path.end();it1++){
+        cv::Size boardSize;
+        cv::Mat image = cv::imread(*it1);
+        boardSize.width = board_width;
+        boardSize.height = board_height;
+        CornerDetection::Chessboard chessboard(boardSize, image);
+        chessboard.findCorners(false);
 
-    bool ret = chessboard.cornersFound();
+        std::vector<cv::Point2f> corners_detected;
+        list<tuple<float,float>> corners_list(board_height*board_width,make_tuple(0,0));
 
-    if (ret)
-    {
-        corners_detected = chessboard.getCorners();
-        cout << "# INFO: Detected chessboard in image " << image_path << std::endl;
+        bool ret = chessboard.cornersFound();
 
-        int i=0;
-        for(auto it = corners_list.begin();it!=corners_list.end();it++){
-            get<0>(*it)= corners_detected[i].x;
-            get<1>(*it)= corners_detected[i].y;
-            i++;
+        if(ret){
+                corners_detected = chessboard.getCorners();
+                cout << "# INFO: Detected chessboard in image " << *it1 << std::endl;
+
+            int i=0;
+            auto it = corners_list.begin();
+            for(it = corners_list.begin();it!=corners_list.end();it++){
+                get<0>(*it)= corners_detected[i].x;
+                get<1>(*it)= corners_detected[i].y;
+                i++;
+            }
+
+            ans.push_back(corners_list);
         }
-
+        else
+        {
+            cout << "# ERROR: Did not detect chessboard in image " << *it1 << std::endl;
+        }
     }
-    else
-    {
-        cout << "# ERROR: Did not detect chessboard in image " << image_path << std::endl;
-    }
-    return corners_list;
-
+    return ans;
 }
