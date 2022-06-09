88ab8b3cc0d3a4b83ac5071c721e9449f20ea85b
rohithreddy1305 <106958996+rohithreddy1305@users.noreply.github.com>
2022-06-09 19:33:51 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,10 +10,14 @@ def test_binding():
 
 def test_cornerdetector():
     print("Sucess From cornerdetector")
+    images_path = []
     file_path = os.path.dirname(os.path.realpath(__file__))
     image_path = os.path.join(file_path,"IMG_3715.jpg")
+    images_path.append(image_path)
+    image_path = os.path.join(file_path,"IMG_3134.jpg")
+    images_path.append(image_path)
     # print("File Path:",image_path)
-    output = pyChessBoardDetector.get_chessboard_corners(image_path,9,7,False)
+    output = pyChessBoardDetector.get_chessboard_corners(images_path,9,7,False)
     # print("output= ",output)
 
 
